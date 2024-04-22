2955f4ec1de1f6c2988ec2ad5d963cfb565c2cf9
Piotr Ranosz <pranosz@wp.pl>
2024-04-22 22:04:37 +0200

    init

---
+++ src/app/page-2/page-2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-page-2',
+  templateUrl: './page-2.component.html',
+  styleUrls: ['./page-2.component.scss']
+})
+export class Page2Component {
+
+}
